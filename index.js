222e4dfedfdfa9bafb3a48d5c852eb0fd07d4368
Chigozie Amaechi <chigozieamaechi@gmail.com>
2022-03-03 03:29:01 -0800

    assignment

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 window.onload = function(){
-    let a = 0
-    let b = 1
-    alert(0)
+    // type: string, integer, bolean, array, objects
+
+    // Example - String
+    let name = "Amaechi Chigozie";
+    let sentence = "This is chigozie's sentence"
+    let password = `123uy`
+
+    // Example - Integer
+    let a = 0;
+
+    // Array example
+    let fruits = ["Apple", "orange", "Pawpaw", "pineaple"];
+   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+   // Example - Bolean
+    let isNight = true;
+    let isGreater = a > 0;
+    console.log(isGreater);
+   
+    // Example - Object
+let obj = {"name": "Chigozie", "state": "imo", "gender": "male"},
+  let attendance = [
+    {"name": "Chigozie", "state": "imo", "gender": "male"}, 
+    {"name": "Chinwe", "state": "Lagos", "gender": "Female"},
+    {"name": "Eze", "state": "Enugu", "gender": "male"},
+  ]  
+
+ console.log(`${obj.state}`)   
+
+
+console.log(fruits[0]); // output will be apple
+    console.log(fruits[1]); // output will be orange
+   
+    
+
+
+
+    // SOLUTION TO ASSIGNMENT
+    // Using for loop, print out the values of each students
+    //for (let attendance = 0; attendance < array.length; attendance++) {
+     // const element = array[attendance];
+      
+    //}
 }
